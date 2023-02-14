bb307ec60b844e79c1568bfde11cae80633bd216
Lollo48 <loory4895@gmail.com>
2023-02-14 10:02:00 +0100

    0.0.2
    add all commentary to the scripts

--- Assets/Script/GenerateMap.cs
+++ Assets/Script/GenerateMap.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class GenerateMap : MonoBehaviour
 {
-    [SerializeField] private int minDistanceFromPlayer;
-    [SerializeField] private int maxTerrainCount;
+    [SerializeField] private int m_minDistanceFromPlayer;
+    [SerializeField] private int m_maxTerrainCount;
     [SerializeField] private List<TerrainData> terrainDatas = new List<TerrainData>();
     [SerializeField] private Transform terrainHolder;
 
@@ -16,34 +16,35 @@ public class GenerateMap : MonoBehaviour
 
     private void Start()
     {
-        for (int i = 0; i < maxTerrainCount; i++)
+        for (int i = 0; i < m_maxTerrainCount; i++)
         {
             SpawnTerrain(true, new Vector3(0,0,0));
         }
-        maxTerrainCount = currentTerrains.Count;
+        m_maxTerrainCount = currentTerrains.Count;
 
     }
 
 
     public void SpawnTerrain(bool isStart, Vector3 playerPos)
     {
-        if ((currentPosition.z - playerPos.z < minDistanceFromPlayer)||(isStart))
+        if ((currentPosition.z - playerPos.z < m_minDistanceFromPlayer)||(isStart))//distancefrom player 
         {
-            int whichTerrain = Random.Range(0, terrainDatas.Count);
-            int terrainInSuccession = Random.Range(1, terrainDatas[whichTerrain].maxInSuccession);
+            int whichTerrain = Random.Range(0, terrainDatas.Count); //take randomly from terrainDatas 
+            int terrainInSuccession = Random.Range(1, terrainDatas[whichTerrain].maxTerrainInSuccession); //take randomly from terrainDatas the succession of it
             for (int i = 0; i < terrainInSuccession; i++)
             {
+                //instantiate a new terrain pick from terrainDatas all randomly and put in a terrainHolder
                 GameObject terrain = Instantiate(terrainDatas[whichTerrain].terrain[Random.Range(0, terrainDatas[whichTerrain].terrain.Count)], currentPosition, Quaternion.identity, terrainHolder);
-                currentTerrains.Add(terrain);
+                currentTerrains.Add(terrain); //add terrain 
                 if (!isStart)
                 {
-                    if (currentTerrains.Count > maxTerrainCount)
+                    if (currentTerrains.Count > m_maxTerrainCount) //control from currentTerrainsCount and maxTerrainCount
                     {
-                        Destroy(currentTerrains[0]);
+                        Destroy(currentTerrains[0]);//destroy currentTerrains 
                         currentTerrains.RemoveAt(0);
                     }
                 }
-                currentPosition.z=currentPosition.z+1;
+                currentPosition.z=currentPosition.z+1; //incrementa z position for the terrains
             }
         }
 
